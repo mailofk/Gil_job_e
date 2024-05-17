22eeadfa63fe0d4f2d8d9853d09b82a5074532a8
mailofk <mailofk@naver.com>
2024-05-17 14:57:43 +0900

    first commit

---
+++ src/main/java/sejong/hackerton/demo/GilljobeApplication.java
@@ -0,0 +1,13 @@
+package sejong.hackerton.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GilljobeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GilljobeApplication.class, args);
+	}
+
+}
