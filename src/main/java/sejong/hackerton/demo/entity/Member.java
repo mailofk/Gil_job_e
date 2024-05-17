202ef9b1cf0085141560d506a285bfa726e42efc
rndnjsgud <00kwh@naver.com>
2024-05-17 17:57:45 +0900

    modify: getter&setter 삭제

--- src/main/java/sejong/hackerton/demo/entity/Member.java
+++ src/main/java/sejong/hackerton/demo/entity/Member.java
@@ -13,7 +13,6 @@ import java.util.List;
 @NoArgsConstructor
 @Data
 public class Member {
-
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
@@ -31,75 +30,9 @@ public class Member {
 
     @Enumerated(EnumType.STRING)
     private Authority authority;
-    public Long getId() {
-        return id;
-    }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
 
-    public String getEmail() {
-        return email;
-    }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
 
-    public String getNickname() {
-        return nickname;
-    }
 
-    public void setNickname(String nickname) {
-        this.nickname = nickname;
-    }
-
-    public Integer getExp() {
-        return exp;
-    }
-
-    public void setExp(Integer exp) {
-        this.exp = exp;
-    }
-
-    public LocalDateTime getCreateAt() {
-        return createAt;
-    }
-
-    public void setCreateAt(LocalDateTime createAt) {
-        this.createAt = createAt;
-    }
-
-    public Integer getLevel() {
-        return level;
-    }
-
-    public void setLevel(Integer level) {
-        this.level = level;
-    }
-
-    public List<TodoList> getTodoLists() {
-        return todoLists;
-    }
-
-    public void setTodoLists(List<TodoList> todoLists) {
-        this.todoLists = todoLists;
-    }
-
-    public List<Message> getMessages() {
-        return messages;
-    }
-
-    public void setMessages(List<Message> messages) {
-        this.messages = messages;
-    }
-
-    public Authority getAuthority() {
-        return authority;
-    }
-
-    public void setAuthority(Authority authority) {
-        this.authority = authority;
-    }
 }
