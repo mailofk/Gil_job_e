1dc2c6ebc7abe1849809a166e5e61f01d5e80bb2
CheolPark98 <96028198+CheolPark98@users.noreply.github.com>
2024-05-17 16:34:16 +0900

    entity 구현

---
+++ src/main/java/sejong/hackerton/demo/entity/Message.java
@@ -0,0 +1,24 @@
+package sejong.hackerton.demo.entity;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+public class Message {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String context;
+    private LocalDateTime timeStamp;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+}
